296e16bdf5e7ebc12bb542d1f87b848e0d8d58bd
Woon Ket Wong <woonketwong@hotmail.com>
2013-10-29 14:58:01 -0700

    Add MongoDB

--- server.js
+++ server.js
@@ -9,13 +9,34 @@ var path = require('path');
 var config = require('./config/config.js');
 var router = require('./routes/router.js');
 var mongoose = require('mongoose');
+var util = require('util');
+
+//Database initialization
 mongoose.connect('mongodb://localhost/test');
+var db = mongoose.connection;
 
-var Cat = mongoose.model('Cat', { name: String });
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+  var jobApplicantSchema = mongoose.Schema({
+    name: String
+  });
+  var JobApplicant = mongoose.model('JobApplicant', jobApplicantSchema);
+  var newJobApplicant = new JobApplicant({name: 'Adam'});
 
-
+  newJobApplicant.save(function (err, data) {
+  	if (err) console.log("ERR!!!");
+  	console.log("DATA!!",data);
+  });
+});
 
 var app = express();
+app.param('collectionName', function(req, res, next, collectionName) {
+  req.collection = db.collection(collectionName);
+  return next();
+});
+
+var app = express();
+
 config(app);
 router(app);
 
