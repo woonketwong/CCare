e53c8cf64a6ddb978ce4d1f29cabc15cce3dd9f1
StephenGrider <ste.grider@gmail.com>
2013-10-29 18:56:52 -0700

    Prototyped user application process

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,11 +1,43 @@
 angular.module('CCare',[])
-  .config(function($routeProvider){
-  $routeProvider.when('/',{
-    controller: 'formCtrl',
-    templateUrl: 'templates/index.html'
-  });
-}).controller('formCtrl',function($scope,$http){
-    $scope.submit = function(){
-      $http.post('/formInput', [$scope.user,$scope.password]);
+  .config(function($routeProvider,$locationProvider){
+  $routeProvider
+    .when('/',{
+          templateUrl: 'templates/index.html'
+        })
+    .when('/worker-registration',{
+          controller: 'reg1Ctrl',
+          templateUrl: 'templates/worker-registration.html'
+        })
+    .when('/worker-registration2',{
+          controller: 'reg2Ctrl',
+          templateUrl: 'templates/worker-registration2.html'
+        })
+    .when('/worker-registration3',{
+          controller: 'reg3Ctrl',
+          templateUrl: 'templates/worker-registration3.html'
+        })
+    .when('/worker-registration4',{
+          controller: 'reg4Ctrl',
+          templateUrl: 'templates/worker-registration4.html'
+        });
+}).controller('reg1Ctrl',function($scope,$http, workerApplication){
+    $scope.processFormData = function(){
+      debugger;
+      workerApplication['username'] = $scope.username;
+      workerApplication['password'] = $scope.password1;
+      workerApplication['email'] = $scope.email;
     };
-  });
+  }).controller('reg2Ctrl',function($scope,$http, workerApplication){
+    
+  }).controller('reg3Ctrl',function($scope,$http, workerApplication){
+    
+  }).controller('reg4Ctrl',function($scope,$http, workerApplication){
+    $scope.submit = function(){
+      console.log('submitted?');
+      console.log(workerApplication);
+      // $http.post('/formInput', [$scope.user,$scope.password]);
+    }
+  
+  }).service('workerApplication', function () {
+        return {};
+    });
