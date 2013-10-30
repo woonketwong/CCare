de0f26b801811be2dc090efb2bdf287d3107fa2d
Hack Reactor Students <github@hackreactor.com>
2013-10-29 19:39:10 -0700

    Add heroku mongoDB addon

--- server.js
+++ server.js
@@ -16,7 +16,18 @@ var app = express();
 ////////////////////////////////////////
 //Database initialization
 ////////////////////////////////////////
-mongoose.connect('mongodb://localhost/test');
+var uristring =
+process.env.MONGOLAB_URI ||
+process.env.MONGOHQ_URL ||
+'mongodb://localhost/HelloMongoose';
+
+mongoose.connect(uristring, function(err, res){
+  if (err) {
+  console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+  console.log ('Succeeded connected to: ' + uristring);
+  }
+});
 // var db = mongoose.connection;
 
 // db.on('error', console.error.bind(console, 'connection error:'));
