6dc1cad576fc613189868f157043f83ddd3d2204
Hack Reactor <hackreactor@Hacks-Mac-mini.local>
2013-10-31 10:55:13 -0700

    Added to database schema

--- server.js
+++ server.js
@@ -35,7 +35,57 @@ mongoose.connect(uristring, function(err, res){
   var jobApplicantSchema = mongoose.Schema({
     name: String,
     email: String,
-    password: String
+    password: String,
+    accountCreated: Boolean,
+    preferences: {
+      hourlyRate: Number, 
+      dailyRate: Number,
+      jobType: {
+        caregiver: Boolean,
+        CHHA: Boolean,
+        STNA: Boolean,
+        PCA: Boolean,
+        LPN: Boolean,
+        CNA: Boolean
+      },
+      //ADD SCHEDULE INFO
+      homeCareInterest: Boolean,
+      facilityCareInterest: Boolean,
+      workRadius: Number,
+      carAvailable: Boolean,
+      yearsExperience: Number,
+      previousEmployerName: String,
+      certifications:{
+        PCA: Boolean,
+        CHHA: Boolean,
+        CNA: Boolean,
+        LPN: Boolean
+      },
+      languages:{
+        Arabic: Boolean,
+        Chinese_Cantonese: Boolean,
+        Chinese_Mandarin: Boolean,
+        Farsi: Boolean,
+        Filipino: Boolean,
+        French: Boolean,
+        Greek: Boolean,
+        Hebrew: Boolean,
+        Hindu: Boolean,
+        Italian: Boolean,
+        Japanese: Boolean,
+        Korean: Boolean,
+        Polish: Boolean,
+        Russian: Boolean,
+        Spanish: Boolean,
+        Swahili: Boolean,
+        Vietnamese: Boolean
+      },
+      specializations:{
+
+      },
+      idealPatient: String,
+      idealWorkEnvironment: String
+    }
   });
 
   // );
