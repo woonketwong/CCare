823e01690e9b2c7a6a829794047cd1d82c77c428
Woon Ket Wong <woonketwong@hotmail.com>
2013-10-30 19:48:34 -0700

    Implement email notifier and hashed password module

--- router.js
+++ router.js
@@ -1,7 +1,9 @@
-var home = require('./routes/index');
+var home = require('./controllers/index.js');
+var user = require('./controllers/user.js');
 
 module.exports = function(app){
   app.get('/', home.index);
   app.post('/worker-signup', home.workerSignup);
+  app.get('/worker-signup', home.workerReadInfo);
   app.get('/collections/:collectionName', home.index);
 };
