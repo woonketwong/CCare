7885aa674c2c588bc4c0c39c36b8dba3ee7befdd
StephenGrider <ste.grider@gmail.com>
2013-11-18 10:22:08 -0800

    Readme updates

--- config/config.js
+++ config/config.js
@@ -8,8 +8,12 @@ module.exports = {
   },
   mailer: {
     auth: {
-       user: "credentialedcaredev@gmail.com",
-       pass: "credentialed"
+      //Note to CCare - these credentials will be visible when the project is uploaded to github.  
+      //You should replace these credentials with another email account then mark this file to NOT be 
+      //uploaded to github via the the .gitignore file, otherwise anyone with access to the repo will be able
+      //to see this login info.  See https://help.github.com/articles/ignoring-files.
+      user: "credentialedcaredev@gmail.com",
+      pass: "credentialed"
     },
    defaultFromAddress: 'Credentialed Care <credentialedcaredev@gmail.com>'
   }
